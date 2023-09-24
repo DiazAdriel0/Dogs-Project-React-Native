2f0f3a8e7ef89418a8f2556d3017e3495315a5e1
DiazAdriel0 <diazadriel0@gmail.com>
2023-09-24 20:46:02 -0300

    fix(components/NavBar): previous NavBar fixed

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,27 +1,12 @@
 import React from 'react'
-import { StyleSheet, View } from 'react-native'
-import { NavigationContainer } from '@react-navigation/native'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { StyleSheet, View, Text } from 'react-native'
 import Constants from 'expo-constants'
-import Home from '../screens/HomeScreen'
-import Landing from '../screens/LandingScreen'
-
-const Stack = createNativeStackNavigator()
 
 const NavBar = () => {
   return (
     <View style={styles.container}>
       <View>
-        <NavigationContainer>
-          <Stack.Navigator>
-            <Stack.Screen
-              name='Home'
-              component={Home}
-              options={{ title: 'Welcome' }}
-            />
-            <Stack.Screen name='Landing' component={Landing} />
-          </Stack.Navigator>
-        </NavigationContainer>
+        <Text>NavBar</Text>
       </View>
     </View>
   )
