95bb679d3091bf0820b69dea1abba239c6bc7820
DiazAdriel0 <diazadriel0@gmail.com>
2023-09-26 11:33:12 -0300

    feat(components/NavBar): buttons added to nav

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,33 @@
 import React from 'react'
-import { StyleSheet, View, Text } from 'react-native'
-import Constants from 'expo-constants'
+import { StyleSheet, View, Text, TouchableOpacity, Image } from 'react-native'
+import icon from './../../assets/favicon.png'
 
-const NavBar = () => {
+const NavBar = ({ navigation }) => {
   return (
     <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <TouchableOpacity
+          style={styles.navButton}
+          onPress={() => navigation.navigate('HomeScreen')}
+        >
+          <Image source={icon} style={styles.image} />
+        </TouchableOpacity>
+      </View>
       <View>
-        <Text>NavBar</Text>
+        <TouchableOpacity
+          style={styles.navButton}
+          onPress={() => navigation.navigate('CreateScreen')}
+        >
+          <Text>Create</Text>
+        </TouchableOpacity>
+      </View>
+      <View>
+        <TouchableOpacity
+          style={styles.navButton}
+          onPress={() => navigation.navigate('AboutMeScreen')}
+        >
+          <Text>About Me</Text>
+        </TouchableOpacity>
       </View>
     </View>
   )
@@ -16,8 +37,18 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: '#c9e4fd',
     alignItems: 'center',
+    justifyContent: 'space-around',
+    height: 50,
+    flexDirection: 'row'
+  },
+  navButton: {
+    height: 40,
     justifyContent: 'center',
-    marginTop: Constants.statusBarHeight
+    paddingHorizontal: 10
+  },
+  image: {
+    height: 40,
+    width: 50
   }
 })
 
